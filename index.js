7f60a30f9196fd669034216abc49f3f8e0776176
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-14 17:58:57 +0000

    Added some comments to describe cb functions in fetching

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+//below demostrating how we relly on call backs to get data that we then use to fetch data from api endpoint and then using another cb to clg that data
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
-      console.log(res.body);
+      console.log(res.body.message);
     });
 });
