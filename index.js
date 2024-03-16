abbf872b7afdf6405a948a2a4583ee4b3e0650da
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-16 08:16:55 +0000

    Rewrote the same logic using async await feature of js

--- index.js
+++ index.js
@@ -20,19 +20,35 @@ const writeFilePro = (file, data) => {
   });
 };
 
+// Async await approach
+
+const getDogPic = async () => {
+  const data = await readFilePro(`${__dirname}/dog.txt`);
+  console.log(`Breed: ${data}`);
+
+  const res = await superagent.get(
+    `https://dog.ceo/api/breed/${data}/images/random`
+  );
+  console.log(res.body.message);
+  await writeFilePro("dog-img.txt", res.body.message);
+  console.log("Random dog image saved to file!");
+};
+
+getDogPic();
+
 //calling the function that will return the promise and so .then handler is available for us.
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
 
-    return writeFilePro("dog-img.txt", res.body.message);
-  })
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
 
-  .then(() => {
-    console.log("Random dog image saved to file!");
-  })
-  .catch((err) => console.log(err.message));
+//   .then(() => {
+//     console.log("Random dog image saved to file!");
+//   })
+//   .catch((err) => console.log(err.message));
